ccd3732247516aafd18d09f1710b6028712f9870
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-04-07 09:45:56 +0200

    Update

--- tests/testrunner/detect.php
+++ tests/testrunner/detect.php
@@ -5,15 +5,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link id="theme" href="css/default/default.css" rel="stylesheet" type="text/css">
-        <style>
-            tr.fail td {
-                background-color: #F78181 !important;
-            }
-
-            pre, .log, tr td {
-                font-size: 0.8em !important;
-            }
-        </style>
     </head>
     <script src="blend/blend.js" type="text/javascript"></script>
     <script>
