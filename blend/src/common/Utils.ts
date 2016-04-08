5931cb352e2765fb80499fb85d989b0e9d8c30bf
Gevik Babakhani <gevik@blendjs.com>
2016-04-08 13:55:45 +0200

    Added ucfirst to string

--- blend/src/common/Utils.ts
+++ blend/src/common/Utils.ts
@@ -2,6 +2,16 @@ interface Array<T> {
     unique(): Array<T>;
 }
 
+interface String {
+    ucfirst(): string;
+}
+
+if (!String.prototype.ucfirst) {
+    String.prototype.ucfirst = function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    };
+}
+
 if (!String.prototype.trim) {
     String.prototype.trim = function() {
         return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
