6b8f9ebe7e51e08310a05469c2839f2653617391
Gevik Babakhani <gevik@blendjs.com>
2016-04-08 11:20:34 +0200

    Added the aplity to call component functions dynamically

--- blend/src/Component.ts
+++ blend/src/Component.ts
@@ -7,9 +7,31 @@ namespace Blend {
 
         protected config: any;
 
-        constructor(config: any) {
+        constructor(config: any = null) {
             var me = this;
             me.config = config || {};
         }
+
+        /**
+         * Check if this Component implements a function
+         */
+        public hasFunction(fname: string) {
+            var me: any = this;
+            return !Blend.isNullOrUndef(me[fname]) && Blend.isFunction(me[fname]);
+        }
+
+        /**
+         * Dynamically run a function within this Component
+         */
+        public applyFunction(name: string, args: Array<any>|IArguments): any {
+            var me: any = this,
+                fn: Function = <Function>me[name];
+            if (Blend.isFunction(fn)) {
+                return fn.apply(me, args);
+            } else {
+                throw new Error(`Class method [${name}] does not exist!`);
+            }
+        }
+
     }
 }
