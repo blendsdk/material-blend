9776e6f609df23f2c18cd6a9d6afba6e09f3f038
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-04-13 01:36:24 +0200

    Implemented the layout cycle

--- blend/src/ui/View.ts
+++ blend/src/ui/View.ts
@@ -15,10 +15,12 @@ namespace Blend.ui {
         protected layoutEnabled: boolean;
         protected layoutTriggers: Array<string>
         private sizeHash: string;
+        private isInALayoutContext: boolean
 
         public constructor(config: UIViewInterface = {}) {
             super(config);
             var me = this;
+            me.isInALayoutContext = false;
             me.sizeHash = null;
             me.parent = config.parent || null;
             me.isRendered = false;
@@ -124,6 +126,36 @@ namespace Blend.ui {
         }
         /////////////////////////////////////////////////////////////////////////
 
+        /**
+         * Makes sure the layout state is invalid so it can be placed in
+         * the next layout cycle
+         */
+        invalidateLayout(performLayout?: boolean) {
+            var me = this;
+            me.sizeHash = null;
+            if (performLayout === true) {
+                me.performLayout();
+            }
+        }
+
+        /**
+         * Initiates a layout cycle based on an event. This function also checks
+         * whether the view is currently part of an existing layout cycle.
+         * If the component is part on a layout cycle then its native performLayout is
+         * called, otherwise if it has a parent then the layout cycle will be deligated
+         * from there.
+         */
+        private handleLayoutTriggers(eventName: string) {
+            var me = this;
+            // only fire and event when the component is rendered and ready
+            if (me.layoutTriggers.indexOf(eventName) !== -1 && me.canLayout()) {
+                if (!me.isInALayoutContext && me.parent) {
+                    (<View>me.parent).invalidateLayout(true);
+                } else {
+                    me.performLayout();
+                }
+            }
+        }
 
         /**
          * Creates and retrives the current size hash on this View
@@ -132,6 +164,44 @@ namespace Blend.ui {
             var me = this,
                 cs = <ElementBoundsInterface>me.getBounds();
             return [cs.height, cs.width].join('-');
+        }
+
+        /**
+         * Performs the actual layout peration on thie View
+         */
+        protected layoutView() {
+
+        }
+
+        /**
+         * Put this View in a parent layout context by passing true
+         * or false otherwise
+         */
+        placeInALayoutContext(state: boolean) {
+            this.isInALayoutContext = state;
+        }
+
+        /**
+         * Initiates a layout cycle on this View
+         */
+        public performLayout() {
+            var me = this;
+            if (me.canLayout()) {
+                me.suspendLayout();
+                if (me.shouldLayout()) {
+                    me.layoutView.apply(me, arguments);
+                }
+                me.resumeLayout();
+            }
+        }
+
+        /**
+         * Checks if the size of this View is different that sizeHash.
+         * If so then this View should be placed in a layout cycle
+         */
+        protected shouldLayout(): boolean {
+            var me = this, cur = me.getSizeHash();
+            return (me.sizeHash !== cur);
         }
 
         /**
@@ -190,6 +260,7 @@ namespace Blend.ui {
             } else {
                 Blend.apply(me.config.style, style, false, true);
             }
+            me.redoLayout();
         }
 
         /**
@@ -202,6 +273,7 @@ namespace Blend.ui {
             } else {
                 Blend.apply(me.config.css, css, false, true);
             }
+            me.redoLayout();
         }
 
         /**
@@ -244,6 +316,15 @@ namespace Blend.ui {
             return me.element.getEl();
         }
 
+        /**
+         * Internal notification to initiate a layout cycle. This method
+         * is used when the View has to relayout but no external event
+         * should be fired
+         */
+        protected redoLayout() {
+            this.fireEvent('redo-layoyt');
+        }
+
         protected fireEvent(eventName: string, ...args: any[]) {
             /**
              * Override of the fireEvent function to trigger
@@ -251,10 +332,10 @@ namespace Blend.ui {
              */
             var me = this;
             if (me.isRendered === true && me.eventsEnabled === true) {
-                //me.handleLayoutTriggers(eventName);
-                //if (eventName !== 'redo-layout') {
-                super.fireEvent.apply(me, arguments);
-                //}
+                me.handleLayoutTriggers(eventName);
+                if (eventName !== 'redo-layout') {
+                    super.fireEvent.apply(me, arguments);
+                }
             }
         }
     }
