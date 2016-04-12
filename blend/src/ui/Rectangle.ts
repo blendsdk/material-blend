01164db0e203b97edeb7c9f48552be58d7517cc2
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-04-13 01:20:16 +0200

    Renamed file

--- blend/src/ui/Rectangle.ts
+++ blend/src/ui/Rectangle.ts
@@ -4,6 +4,8 @@
 namespace Blend.ui {
 
     export interface RectangleConfig extends UIViewInterface {
+        color?: string
+        border?: boolean
     }
 
     export class Rectangle extends Blend.ui.View {
@@ -13,6 +15,15 @@ namespace Blend.ui {
             super(config);
             var me = this;
             me.cssClass.push('rectangle');
+            me.setBounds({
+                width: config.width || 100,
+                height:config.height || 100
+            })
+            me.setStyle({
+                'background-color': config.color || 'transparent',
+                'border': config.border == true ? '1px solid #000' : null,
+                display: 'inline-block'
+            });
         }
 
     }
