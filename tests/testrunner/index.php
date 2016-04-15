0c3f752e0693584ece1c1927fd439efede2fa00f
Gevik Babakhani <gevik@blendjs.com>
2016-04-15 16:31:21 +0200

    Fore reloading the CSS files!

--- tests/testrunner/index.php
+++ tests/testrunner/index.php
@@ -24,7 +24,7 @@ function getTestFiles() {
         <title>BlendJS Test Runner</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link id="theme" href="css/default/default.css" rel="stylesheet" type="text/css">
+		<link id="theme" href="css/default/default.css?v=<?php echo time();?>" rel="stylesheet" type="text/css">
         <style>
             tr.fail td {
                 background-color: #F78181 !important;
