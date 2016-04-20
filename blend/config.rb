753f66b204b1c87346302c68d9050a416a0785cc
Gevik Babakhani <gevik@blendjs.com>
2016-04-20 14:13:58 +0200

    Added Font-Awesome 4.6.1

--- blend/config.rb
+++ blend/config.rb
@@ -1,6 +1,7 @@
 require 'compass/import-once/activate'
 http_path = "/"
 css_dir = "../build/css"
+fonts_dir = "../build/css/fonts"
 sass_dir = "themes"
 images_dir = "images"
 javascripts_dir = "js"
