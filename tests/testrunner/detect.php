b8344787fc4976fcabd0d999904c143dacf7af4c
Gevik Babakhani <gevik@blendjs.com>
2016-04-25 18:12:27 +0200

    added grid experimental

--- tests/testrunner/detect.php
+++ tests/testrunner/detect.php
@@ -6,18 +6,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link id="theme" href="css/default/default.css" rel="stylesheet" type="text/css">
         <style>
-            #host {
+            .host {
                 position:absolute;
                 top:100px;
                 left:100px;
                 width:400px;
                 height:400px;
                 background-color:#ededed;
+                border:1px solid gray;
+            }
+
+            .row {
+                background-color:orange;
+            }
+
+            div {
+                border:1px dotted gray;
             }
         </style>
     </head>
     <script src="blend/blend.js" type="text/javascript"></script>
-    <script src="js/experiments/exp1.js" type="text/javascript"></script>
+    <!--<script src="js/experiments/exp1.js" type="text/javascript"></script>-->
     <body>
+        <div class="row">
+            <div class="col-lg-6">lG6</div>
+            <div class="col-lg-6">lG6</div>
+        </div>
     </body>
 </html>
