593aad7010d53bef7f659282d7766d51ec559884
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-05-22 22:58:42 +0200

    Implemented MD typography specs

--- tests/testrunner/develop.php
+++ tests/testrunner/develop.php
@@ -5,11 +5,27 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link id="theme" href="css/default/default.css" rel="stylesheet" type="text/css">
-        <script src="blend/support.js"></script>
         <script src="blend/blend.js"></script>
-        <script src="js/experiments/exp1.js"></script>
     </head>
-    <body class="default">
-        Please wait....
+    <body class="material default">
+        <div style="">
+            <div class="display4">Light 112sp</div>
+            <div class="display3">Regular 56sp</div>
+            <div class="display2">Regular 45sp</div>
+            <div class="display1">Regular 34sp</div>
+            <div class="headline">Regular 24sp</div>
+            <div class="title">Medium 20sp</div>
+            <div class="subhead">Subheading 16sp</div>
+            <div class="body2">Medium 14sp</div>
+            <div class="body1">Regular 14sp</div>
+            <div class="caption">Regular 12sp</div>
+            <hr/>
+            <p style="width:500px" class="">
+                Language scripts that require extra line height to accommodate larger glyphs, including South and Southeast Asian and Middle Eastern languages, like Arabic, Hindi, Telugu, Thai, Vietnamese.
+            </p>
+        </div>
+        <!--        <div>
+                    <img style="width:760px;height:568px"src="https://material-design.storage.googleapis.com/publish/material_v_8/material_ext_publish/0Bzhp5Z4wHba3alhXZ2pPWGk3Zjg/style_typography_styles_scale.png"/>
+                </div>-->
     </body>
 </html>
