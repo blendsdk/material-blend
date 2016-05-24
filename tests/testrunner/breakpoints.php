2052f110e9474486b5cd24814c87afc791497451
Gevik Babakhani <gevik@blendjs.com>
2016-05-24 13:05:36 +0200

    Added a responsive grid system

--- tests/testrunner/breakpoints.php
+++ tests/testrunner/breakpoints.php
@@ -8,28 +8,52 @@
         <script src="blend/support.js"></script>
         <script src="blend/blend.js"></script>
         <style>
-            .flex-grid > div {
-                background-color: gray;
-                padding: 5px;
-                color:#fff;
+            .default .grid {
+                background-color: burlywood;
+                margin-bottom: 10px;
             }
 
-            .flex-grid {
-                xmargin-bottom: 10px;
-                margin: 10px;
+            .default .grid .cell {
+                background-color: lightgrey;
+            }
+
+            .default .grid5 {
+                background-color: chocolate !important;
             }
         </style>
     </head>
     <body class="material default">
-        <div class="flex-grid">
-            <div class="flex-item flex-1-4 flex-1-8 flex-3-12">Item 1</div>
-            <div class="flex-item flex-1-4 flex-1-8 flex-3-12">Item 2</div>
-            <div class="flex-item flex-1-4 flex-2-8 flex-3-12">Item 3</div>
-            <div class="flex-item flex-1-4 flex-4-8 flex-3-12">Item 4</div>
-        </div>
-        <div class="flex-grid">
-            <div class="flex-item flex-2-4 flex-4-8">Item 1</div>
-            <div class="flex-item flex-2-4 flex-4-8">Item 2</div>
-        </div>
     </body>
+    <script>
+        var body = Blend.selectElement('body');
+        var gindex = 1;
+        var createGrid = function (columns) {
+            var cp = new Blend.dom.ElementConfigBuilder();
+            cp.addCSS(['grid', 'grid' + gindex]);
+            var index = 1;
+            columns.forEach(function (w) {
+                cp.addChild({
+                    cls: ['cell cell-' + w],
+                    text: 'Cell ' + w
+                });
+                index++;
+            });
+            body.append(Blend.createElement(cp));
+            gindex++;
+        }
+        createGrid([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+        createGrid([2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+        createGrid([3, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+        createGrid([4, 1, 1, 1, 1, 1, 1, 1, 1]);
+        createGrid([5, 1, 1, 1, 1, 1, 1, 1]);
+        createGrid([6, 1, 1, 1, 1, 1, 1]);
+        createGrid([7, 1, 1, 1, 1, 1]);
+        createGrid([8, 1, 1, 1, 1]);
+        createGrid([9, 1, 1, 1]);
+        createGrid([10, 1, 1]);
+        createGrid([11, 1]);
+        createGrid([12]);
+
+        Blend.selectElement('.grid5').addCssClass('grid-no-spacing');
+    </script>
 </html>
