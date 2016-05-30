f91ffb2c439c0a84fb1a90d526e2d3d78cb1eb75
Gevik Babakhani <gevik@blendjs.com>
2016-05-30 12:13:10 +0200

    synchronized the tests

--- tests/tests/300010-ripple-tests.ts
+++ tests/tests/300010-ripple-tests.ts
@@ -15,19 +15,19 @@ TestApp.defineTest('Ripple functions CSS', function(t: Blend.testing.TestRunner)
     var r = new RippleTest({
         element: el
     });
-    t.assertEquals(r.getColor(), 'rgba(0,0,0,0.2)');
+    t.assertEquals(r.getColor(), 'rgba(0,0,0,0.95)');
 
     var r = new RippleTest({
         element: el,
         color:'#fffffe'
     });
-    t.assertEquals(r.getColor(), 'rgba(255,255,254,0.2)');
+    t.assertEquals(r.getColor(), 'rgba(255,255,254,0.95)');
 
     var r = new RippleTest({
         element: el,
         color:'yellow'
     });
-    t.assertEquals(r.getColor(), 'rgba(0,0,0,0.2)');
+    t.assertEquals(r.getColor(), 'rgba(0,0,0,0.95)');
 
     t.done();
 
