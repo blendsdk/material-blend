3663abb98052296993565024854aa81272542f16
Gevik babakhani <gevik@blendjs.com>
2016-05-31 13:54:54 +0200

    Added code comments and implemented methods for check the version of TSC and Compass

--- bin/build.js
+++ bin/build.js
@@ -15,15 +15,20 @@
  * limitations under the License.
  */
 
-
 const fs = require('fs');
 const del = require('del');
 const path = require('path');
 const childProcess = require('child_process');
 const uglify = require("uglify-js");
+const vercompare = require('version-comparison');
 
+/**
+ * Builder class constructor
+ */
 function Builder() {
     var me = this;
+    me.minCompassVersion = '1.0.3';
+    me.minTypeScriptVersion = '1.8.10';
     me.rootFolder = fs.realpathSync(__dirname + '/..');
     me.buildPath = me.rootFolder + '/build';
     me.blendPath = me.rootFolder + '/blend';
@@ -48,19 +53,60 @@ function Builder() {
     ]
 }
 
-Builder.prototype.checkSanity = function(callback) {
+/**
+ * Checks if compass exists and it is the correct version.
+ */
+Builder.prototype.checkCompassSanity = function (callback) {
     var me = this;
     console.log('-- Checking the build environment');
-    childProcess.exec('compass -v', { cwd: me.rootFolder }, function(error, stdout, stderr) {
+    childProcess.exec('compass -v', { cwd: me.rootFolder }, function (error, stdout, stderr) {
         if (!error) {
-            callback.apply(me, [null]);
+            parts = stdout.split("\n");
+            if (parts.length < 1) {
+                callback.apply(me, ['Could not recognize Compass!']);
+            }
+            parts = parts[0].split(' ');
+            if (parts.length !== 3) {
+                callback.apply(me, ['Could not read Compass version!']);
+            }
+            if (vercompare(parts[1], me.minCompassVersion) !== -1) {
+                callback.apply(me, [null]);
+            } else {
+                console.log('Invalid Compass version! Found ' + parts[1] + ', but we require as least ' + me.minCompassVersion)
+            }
         } else {
             callback.apply(me, ['No Compass installation found!']);
         }
     });
 }
 
-Builder.prototype.cleanBuild = function(callback) {
+/**
+ * Checks if TypeScript exists and it is the correct version.
+ */
+Builder.prototype.checkTypeScriptSanity = function (callback) {
+    var me = this;
+    console.log('-- Checking the build environment');
+    childProcess.exec('tsc -v', { cwd: me.rootFolder }, function (error, stdout, stderr) {
+        if (!error) {
+            parts = stdout.trim().split(" ");
+            if (parts.length != 2) {
+                callback.apply(me, ['Could not recognize TypeScript!']);
+            }
+            if (vercompare(parts[1], me.minTypeScriptVersion) !== -1) {
+                callback.apply(me, [null]);
+            } else {
+                console.log('Invalid TypeScript version! Found ' + parts[1] + ', but we require as least ' + me.minTypeScriptVersion)
+            }
+        } else {
+            callback.apply(me, ['No TypeScript installation found!']);
+        }
+    });
+}
+
+/**
+ * Cleanup the build folder. (Delete and Recreate and empty build folder!)
+ */
+Builder.prototype.cleanBuild = function (callback) {
     var me = this;
     try {
         console.log('-- Cleaning the build (' + me.buildPath + ')');
@@ -76,10 +122,13 @@ Builder.prototype.cleanBuild = function(callback) {
     }
 }
 
-Builder.prototype.buildStyles = function(callback) {
+/**
+ * Build the themes and styles used external Compass
+ */
+Builder.prototype.buildStyles = function (callback) {
     var me = this;
     console.log('-- Building Themes and Styles');
-    childProcess.exec('compass compile', { cwd: me.blendPath }, function(error, stdout, stderr) {
+    childProcess.exec('compass compile', { cwd: me.blendPath }, function (error, stdout, stderr) {
         if (!error) {
             callback.apply(me, [null]);
         } else {
@@ -88,10 +137,13 @@ Builder.prototype.buildStyles = function(callback) {
     });
 }
 
-Builder.prototype.buildBlend = function(callback) {
+/**
+ * Build the TS sources, both framework and tests
+ */
+Builder.prototype.buildBlend = function (callback) {
     var me = this;
     console.log('-- Building Blend');
-    childProcess.exec('tsc', { cwd: me.blendPath }, function(error, stdout, stderr) {
+    childProcess.exec('tsc', { cwd: me.blendPath }, function (error, stdout, stderr) {
         if (!error) {
             callback.apply(me, [null]);
         } else {
@@ -99,15 +151,14 @@ Builder.prototype.buildBlend = function(callback) {
         }
     });
 }
-
 
 /**
  * Run a series of functions sequentially and when done call the whenDone callback
  */
-Builder.prototype.runSerial = function(callbacks, whenDone) {
+Builder.prototype.runSerial = function (callbacks, whenDone) {
     var me = this;
-    var makeCall = function(fn, cb) {
-        return function(error) {
+    var makeCall = function (fn, cb) {
+        return function (error) {
             if (!error) {
                 if (cb) {
                     fn.apply(me, [cb]);
@@ -127,15 +178,19 @@ Builder.prototype.runSerial = function(callbacks, whenDone) {
     lastCall.apply(me, []);
 }
 
-Builder.prototype.buildFramework = function() {
+/**
+ * Builds the framework and prepares files for distribution
+ */
+Builder.prototype.buildFramework = function () {
     var me = this;
 
-    var done = function() {
+    var done = function () {
         console.log('-- Done');
     }
 
     me.runSerial([
-        me.checkSanity
+        me.checkTypeScriptSanity,
+        me.checkCompassSanity
         , me.cleanBuild
         , me.buildStyles
         , me.buildBlend
@@ -143,18 +198,21 @@ Builder.prototype.buildFramework = function() {
 
 }
 
-Builder.prototype.copyrightFiles = function(folder, extensions) {
+/**
+ * Places copyright headers in source files
+ */
+Builder.prototype.copyrightFiles = function (folder, extensions) {
     console.log('-- Looking for files');
     var me = this,
         count = 0,
         header = me.copyrightHeader.join("\n");
     extensions = extensions || ['.ts', 'scss'];
-    files = me.readFiles(folder, function(fname) {
+    files = me.readFiles(folder, function (fname) {
         var ext = path.extname(fname);
         return extensions.indexOf(ext) !== -1
             && fname.indexOf(path.sep + 'typings' + path.sep) === -1;
     });
-    files.forEach(function(fname) {
+    files.forEach(function (fname) {
         var contents = fs.readFileSync(fname).toString();
         if (contents.indexOf('Copyright 2016 TrueSoftware B.V') === -1) {
             contents = header + "\n\n" + contents;
@@ -166,14 +224,18 @@ Builder.prototype.copyrightFiles = function(folder, extensions) {
     console.log('-- Done\n');
 }
 
-Builder.prototype.readFiles = function(dir, filter) {
+/**
+ * Recursively reads files from a given folder and applies a filter to
+ * be able to exclude some files
+ */
+Builder.prototype.readFiles = function (dir, filter) {
     var me = this,
         results = [];
-    filter = filter || function(fname) {
+    filter = filter || function (fname) {
         return true;
     }
     var list = fs.readdirSync(dir)
-    list.forEach(function(file) {
+    list.forEach(function (file) {
         file = dir + '/' + file
         var stat = fs.statSync(file)
         if (stat && stat.isDirectory()) {
@@ -187,11 +249,17 @@ Builder.prototype.readFiles = function(dir, filter) {
     return results;
 }
 
-Builder.prototype.fixPath = function(path) {
+/**
+ * Make sure paths are consistent with the OS
+ */
+Builder.prototype.fixPath = function (path) {
     return path.replace('/', path.sep);
 }
 
-Builder.prototype.run = function() {
+/**
+ * Build entry point
+ */
+Builder.prototype.run = function () {
     console.log("MaterialBlend Framework Builder v1.0\n");
     var me = this,
         argv = require('yargs')
