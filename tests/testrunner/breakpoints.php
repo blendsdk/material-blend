e5d0a8e4be7cf191f084ba5acf1e0290a94a5aa2
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-06-18 23:23:58 +0200

    Fixed the breakpoint experiment

--- tests/testrunner/breakpoints.php
+++ tests/testrunner/breakpoints.php
@@ -4,7 +4,7 @@
         <title>BlendJS Test Runner</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link id="theme" href="css/default/default.css" rel="stylesheet" type="text/css">
+        <link id="theme" href="blend/css/default/default.css" rel="stylesheet" type="text/css">
         <script src="blend/support.js"></script>
         <script src="blend/blend.js"></script>
         <style>
