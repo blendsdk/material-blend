72805abf0c3e34fdc023af212b9d6ea0737205d8
Gevik babakhani <gevik@blendjs.com>
2016-06-29 15:47:11 +0200

    Initial implementation of toolbars

---
+++ blend/src/toolbar/Toolbar.ts
@@ -0,0 +1,31 @@
+interface ToolbarInterface extends ContainerInterface {
+    type?: string;
+    size?: string | number;
+}
+
+namespace Blend.toolbar {
+
+    export class Toolbar extends Blend.container.HorizontalBox {
+
+        protected config: ToolbarInterface;
+
+        public constructor(config: ToolbarInterface = {}) {
+            super(Blend.apply(config, <BoxContainerInterface>{
+                align: Blend.eBoxAlign.center,
+                pack: Blend.eBoxPack.start
+            }));
+            var me = this;
+        }
+
+        protected finalizeRender(config: FinalizeRenderConfig = {}) {
+            var me = this;
+            me.addCssClass("mb-toolbar-" + me.config.type);
+            me.setBounds({
+                width: me.config.size
+            });
+            super.finalizeRender(config);
+        }
+    }
+}
+
+Blend.registerClassWithAlias("mb.toolbar", Blend.toolbar.Toolbar);
