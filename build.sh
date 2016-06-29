1b641581d05bf58b2816dcabe888bd92e70b7811
Gevik babakhani <gevik@blendjs.com>
2016-06-29 11:24:27 +0200

    build the examples again

--- build.sh
+++ build.sh
@@ -6,19 +6,19 @@ cd builder
 cd ..
 bin/blend "$@"
 
-# demos=`realpath ../material-blend-dev-demo`
-# if [ -d $demos ]; then
-#     rm -fR $demos/web/blend
-#     rm -fR $demos/themes
-#     rm -fR $demos/web/js
-#     rm -fR $demos/web/css
-#     mkdir -p $demos/web/blend/css
-#     mkdir -p $demos/web/blend/js
-#     cp -fR build/css $demos/web/blend
-#     cp -fR build/blend/blend.js $demos/web/blend/js/blend-debug.js
-#     cp -fR build/blend/blend.d.ts $demos/typings/
-#     cp -fR blend/themes $demos/themes
-#     cd $demos
-#     tsc
-#     compass compile
-# fi
+demos=`realpath ../material-blend-dev-demo`
+if [ -d $demos ]; then
+    rm -fR $demos/web/blend
+    rm -fR $demos/themes
+    rm -fR $demos/web/js
+    rm -fR $demos/web/css
+    mkdir -p $demos/web/blend/css
+    mkdir -p $demos/web/blend/js
+    cp -fR build/css $demos/web/blend
+    cp -fR build/blend/blend.js $demos/web/blend/js/blend-debug.js
+    cp -fR build/blend/blend.d.ts $demos/typings/
+    cp -fR blend/themes $demos/themes
+    cd $demos
+    tsc
+    compass compile
+fi
