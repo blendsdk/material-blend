1cc0d614c3c97df46b2b602dba6fe7fbc204547d
Gevik babakhani <gevik@blendjs.com>
2016-08-02 14:32:18 +0200

    Savepoint

--- blend/src/toolbar/Toolbar.ts
+++ blend/src/toolbar/Toolbar.ts
@@ -11,16 +11,26 @@ interface ToolbarInterface {
     extenedGroup: ToolbarGroupInterface | Blend.toolbar.ToolbarGroup;
 }
 
-
-namespace Blend {
-
-
-
-}
-
 namespace Blend.toolbar {
 
-    export class ToolbarGroupCollection {
+    export class ToolbarGroupCollection implements CollectionProviderInterface<Blend.material.Material> {
+
+        protected collection: Blend.Collection<Blend.material.Material>;
+
+        clearItems: () => void;
+        countItems: () => number;
+        indexOf: (item: Blend.material.Material) => number;
+        getItems: () => Array<Blend.material.Material>;
+        itemAtIndex: (index: number) => Blend.material.Material;
+        add: (item: Blend.material.Material | Array<Blend.material.Material>) => void;
+        remove: (item: Blend.material.Material | number) => void;
+        insertAt: (index: number, item: Blend.material.Material) => void;
+        forEach: (callback: (item: Blend.material.Material, index?: number) => void) => void;
+
+        public constructor() {
+            var me = this;
+            me.collection = new Blend.Collection([], me);
+        }
 
     }
 
